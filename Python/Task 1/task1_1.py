32f39d41186a55a64b2de93788097eb39f39421c
Mostafa Hamdy El-bahrawy <100237567+mostafabahrawy@users.noreply.github.com>
2024-06-13 19:45:59 +0300

    Add files via upload

--- Python/Task 1/task1_1.py
+++ Python/Task 1/task1_1.py
@@ -1,1 +1,7 @@
+numbers = [1, 4, 4, 8, 9, -1, 0, 9, 4]
+count = 0
+for i in numbers:
+    if i == 4:
+        count += 1
 
+print('The number 4 appears ' + str(count) + ' times in the list')
---
+++ Python/Task 1/task1_2.py
@@ -0,0 +1,8 @@
+letter = input('Enter a letter: ')
+
+vowels = ('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u')
+
+if letter in vowels:
+    print('The letter is vowel')
+else:
+    print('The letter is not vowel')
