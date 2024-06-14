1b3fe70dc78e59b676f3817228411dbf56362ac8
Mostafa Hamdy El-bahrawy <100237567+mostafabahrawy@users.noreply.github.com>
2024-06-14 14:29:59 +0300

    Update task1_1.py

--- Python/Task 1/task1_1.py
+++ Python/Task 1/task1_1.py
@@ -1,7 +1,4 @@
 numbers = [1, 4, 4, 8, 9, -1, 0, 9, 4]
-count = 0
-for i in numbers:
-    if i == 4:
-        count += 1
+count = numbers.count(4) 
 
 print('The number 4 appears ' + str(count) + ' times in the list')
